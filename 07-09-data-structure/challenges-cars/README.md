06fe3164d4d1eab88e2f1217d2de105670a9bf4e
parnpresso <parnpresso@gmail.com>
2019-01-07 01:30:03 +0700

    Add Day 8

---
+++ 07-09-data-structure/challenges-cars/README.md
@@ -0,0 +1,19 @@
+# Bite 21. Query a nested data structure
+
+Given the provided cars dictionary:
+
+Get all Jeeps
+Get the first car of every manufacturer.
+Get all vehicles containing the string Trail in their names (should work for other grep too)
+Sort the models (values) alphabetically
+See the docstrings and tests for more details. Have fun!
+
+Update 18th of Sept 2018: as concluded in the forum it is better to pass the cars dict into each function to make its scope local.
+
+Ref: https://codechalleng.es/bites/21/
+
+----------
+
+# Learn Today
+- Easy way to loop concat string with  comma `', '.join(['My', 'name', 'is', 'parn'])`
+- 2 Short Loop `[y for x in xs for y in ys if 'something' in y]`
---
+++ 07-09-data-structure/challenges-cars/cars.py
@@ -0,0 +1,34 @@
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma  + space (', ') separated string of jeep models (original order)"""
+    return ', '.join(cars['Jeep'])
+
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    return [car[0] for car in cars.values()]
+
+
+def get_all_matching_models(cars=cars, grep='trail'):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    matched_cars = [model for car in cars.values() for model in car if grep.lower() in model.lower()]
+    matched_cars.sort()
+    return matched_cars
+
+
+
+def sort_car_models(cars=cars):
+    """sort the car models (values) and return the resulting cars dict"""
+    [model.sort() for model in cars.values()]
+
+    return cars
